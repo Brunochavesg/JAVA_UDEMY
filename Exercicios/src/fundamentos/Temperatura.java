9c3e4efc39fd8db78ff7b0acd478e32d6db519d8
Bruno Chaves <brunochavesg@gmail.com>
2020-05-13 01:06:07 -0300

    Atualização

--- Exercicios/src/fundamentos/Temperatura.java
+++ Exercicios/src/fundamentos/Temperatura.java
@@ -5,12 +5,15 @@ public class Temperatura {
 	public static void main(String[] args) {
 		// (ºF - 32) x 5/9 = Cº
 		final double AJUSTE = 32;
-		final double MULT = 5.0/9.0;
-		double f;
-		double C;
+		final double FATOR = 5.0/9.0;
 		
+		double f = 86;
+		double c =  (f - AJUSTE) * FATOR;
+		System.out.println("O Valor em Celsius é: " + c);
 		
-		System.out.println("Digite um valor em Fº: ");
+		f = 150;
+		c =  (f - AJUSTE) * FATOR;
+		System.out.println("O Valor em Celsius é: " + c);
 		
 			
 		
