95e4364a420906510758f4df512a4c8f92159684
Bruno Chaves <brunochavesg@gmail.com>
2020-05-25 18:49:18 -0300

    Novos Exercicios

--- Exercicios/src/fundamentos/DesafioBruno01.java
+++ Exercicios/src/fundamentos/DesafioBruno01.java
@@ -8,27 +8,30 @@ public class DesafioBruno01 {
 		
 		String nome;
 		byte idade;
-		double salario;
-		double meses;
+		double salario, meses;
 		String palavra;
 		
 		
 				
 		System.out.println("***Bem Vindo!***");
-		System.out.print("Digite seu Nome!: ");
+		System.out.println("Digite seu Nome: ");
 		nome = input.next();
 		System.out.println("Bem vindo Sr(s): " + nome);
-		System.out.print("Qual a sua Idade?: ");
+		
+		System.out.println("Qual a sua Idade?: ");
 		idade = input.nextByte();
-		System.out.print("Qual o seu Salário Atualmente?: ");
+		
+		System.out.println("Qual o seu Salário Atualmente?: ");
 		salario = input.nextDouble();
-		System.out.print("Quantos meses você trabalhou com o salário de " + salario + "?");
+		
+		System.out.printf("Quantos meses você trabalhou com o salário de R$%.4s Reais.", salario);
 		meses = input.nextDouble();
-		System.out.print("Você recebeu o total de: " + salario * meses);
+		
+		System.out.println("Você recebeu o total de:\n " + salario * meses + " ");
+		
 		System.out.println("Escreva uma palavra qualquer: ");
 		palavra = input.next();
-		
-		
+		System.out.println("Sua palavra em Maiusculo!:" + palavra.toUpperCase());
 		
 		
 				
